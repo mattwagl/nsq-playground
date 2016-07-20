eeb9247e788e8ee49a95299cf28b18e403afd41b
Matthias Wagler <wagler@intuity.de>
2016-07-20 20:06:01 +0200

    Add a real master.

--- work-queues/master.js
+++ work-queues/master.js
@@ -1,12 +1,24 @@
 var nsq = require('nsqjs');
 
-var reader = new nsq.Reader('work-queue', 'worker-channel', {
-  lookupdHTTPAddresses: '127.0.0.1:4161'
+var w = new nsq.Writer('127.0.0.1', 4150);
+
+w.connect();
+
+w.on('ready', function () {
+  w.publish('sample_topic', 'it really tied the room together');
+
+  w.publish('sample_topic', [
+    'Uh, excuse me. Mark it zero. Next frame.',
+    'Smokey, this is not \'Nam. This is bowling. There are rules.'
+  ]);
+
+  w.publish('sample_topic', 'Wu?', function (err) {
+    if (err) { return console.error(err.message); }
+    console.log('Message sent successfully');
+    w.close();
+  });
 });
 
-reader.connect();
-
-reader.on('message', function (msg) {
-  console.log('Received message [%s]', msg.id, msg.body.toString());
-  msg.finish();
+w.on('closed', function () {
+  console.log('Writer closed');
 });
